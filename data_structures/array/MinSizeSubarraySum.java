9120832c653c9d0f0d6b430a1c9227924fb13e98
Pramod Maurya <pramod.maurya@sixt.com>
2020-07-29 10:57:18 +0530

    product of array excepot self and min size subarray sum

---
+++ data_structures/array/MinSizeSubarraySum.java
@@ -0,0 +1,31 @@
+package array;
+
+public class MinSizeSubarraySum {
+    /*
+    Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s.
+    If there isn't one, return 0 instead.
+
+    For example, given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal length of 2 under the problem constraint.
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        int result = nums.length;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        while (i < nums.length) {
+            if (sum < s) {
+                sum += nums[i++];
+            } else {
+                result = Math.min(result, i-j);
+                sum -= nums[j++];
+            }
+        }
+
+        while (sum >= s) {
+            result = Math.min(result, i-j);
+            sum -= nums[j++];
+        }
+
+        return result;
+    }
+}
